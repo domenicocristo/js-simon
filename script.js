8e2e47491fc45ec33cdf509af0c1f0545ac58fbc
MyUsername <my@email.com>
2021-11-09 14:46:01 +0100

    inizio

---
+++ script.js
@@ -0,0 +1,5 @@
+// 1. far apparire in pagina 5 numeri casuali 
+// 1.2 da li far partire un timer di 30 secondi 
+// 2. dopo 30 secondi l'utente deve inserire i numeri visti precedentemente 
+// 3. dare un riscontro dei numeri indovinati
+
