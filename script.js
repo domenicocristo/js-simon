aefe1f3b4caa184f27d6427ece42dc94c44229c6
MyUsername <my@email.com>
2021-11-09 16:08:38 +0100

    commit

--- script.js
+++ script.js
@@ -3,3 +3,34 @@
 // 2. dopo 30 secondi l'utente deve inserire i numeri visti precedentemente 
 // 3. dare un riscontro dei numeri indovinati
 
+let containerNumbers = document.getElementById("container-numbers");
+let randomNumbers = [];
+let userNumbers = [];
+
+for (let i = 0; i < 5; i++) {
+    var num = Math.floor(Math.random() * 100);
+    randomNumbers.push(num);
+
+    setTimeout(myFunction, 3000);
+    function myFunction() {
+        document.getElementById('container-numbers').style.display='none';
+        let userNum =  prompt("Inserisci numero");
+        userNumbers.push(userNum);
+    }
+}
+containerNumbers.innerHTML = randomNumbers;
+
+let feedback = document.getElementById("feedback");
+let numValidator = false;
+
+for (let i = 0; i < userNumbers.length; i++) {
+    if (userNumbers == randomNumbers[i]) {
+        numValidator = true;
+    }
+}
+
+if (numValidator === true) {
+    feedback.innerHTML = num;
+} else {
+
+}
